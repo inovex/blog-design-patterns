eeed1a3d2dc602f8773bc712ed5ad4969bf0ded3
Daniel Eckelt <crymis8@gmail.com>
2024-06-21 16:22:35 +0200

    feat: remove link reference between examples, rename to history

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
 
 /** @typedef {'Open' | 'Paid'} Payment */
 
-/** @typedef {{ name: string; ingredients: string | string[]; orderId: string; paymentType: Payment; executedBy: ExecutedBy, description: string; image: string; }} Coffee */
+/** @typedef {{ name: string; quantity: number; ingredients: string | string[]; orderId: string; paymentType: Payment; executedBy: ExecutedBy, description: string; image: string; }} Coffee */
 
 /** @typedef {Coffee[]} CoffeeList */
 
@@ -12,6 +12,7 @@
 const coffeeList = [
   {
     name: "Café Latte",
+    quantity: 2,
     ingredients: ["Robusta", "Soya milk"],
     orderId: "#33467",
     paymentType: "Paid",
@@ -26,6 +27,7 @@ const coffeeList = [
   },
   {
     name: "Espresso",
+    quantity: 1,
     ingredients: "Arabica",
     orderId: "#12532",
     paymentType: "Open",
@@ -40,6 +42,7 @@ const coffeeList = [
   },
   {
     name: "Flat White",
+    quantity: 4,
     ingredients: ["Arabica", "Slow brew", "Oat milk"],
     orderId: "#34642",
     paymentType: "Paid",
@@ -54,6 +57,7 @@ const coffeeList = [
   },
   {
     name: "Espresso",
+    quantity: 9,
     ingredients: "Arabica",
     orderId: "#12532",
     paymentType: "Open",
@@ -68,6 +72,7 @@ const coffeeList = [
   },
   {
     name: "Espresso",
+    quantity: 2,
     ingredients: "Arabica",
     orderId: "#12532",
     paymentType: "Open",
@@ -82,6 +87,7 @@ const coffeeList = [
   },
   {
     name: "Flat White",
+    quantity: 1,
     ingredients: ["Arabica", "Slow brew", "Oat milk"],
     orderId: "#34642",
     paymentType: "Paid",
@@ -96,6 +102,7 @@ const coffeeList = [
   },
   {
     name: "Café Latte",
+    quantity: 1,
     ingredients: ["Robusta", "Soya milk"],
     orderId: "#33467",
     paymentType: "Paid",
@@ -110,6 +117,7 @@ const coffeeList = [
   },
   {
     name: "Flat White",
+    quantity: 2,
     ingredients: ["Arabica", "Slow brew", "Oat milk"],
     orderId: "#34642",
     paymentType: "Paid",
@@ -124,6 +132,7 @@ const coffeeList = [
   },
   {
     name: "Espresso",
+    quantity: 1,
     ingredients: "Arabica",
     orderId: "#12532",
     paymentType: "Open",
@@ -138,6 +147,7 @@ const coffeeList = [
   },
   {
     name: "Flat White",
+    quantity: 3,
     ingredients: ["Arabica", "Slow brew", "Oat milk"],
     orderId: "#34642",
     paymentType: "Paid",
@@ -152,6 +162,7 @@ const coffeeList = [
   },
   {
     name: "Espresso",
+    quantity: 5,
     ingredients: "Arabica",
     orderId: "#12532",
     paymentType: "Open",
@@ -164,165 +175,36 @@ const coffeeList = [
     description:
       "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso.",
   },
-  /*
-    {
-        "name": "Flat White",
-        "ingredients": [
-            "Arabica",
-            "Slow brew",
-            "Oat milk"
-        ],
-        "orderId": "#34642",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Olivia Rhye",
-            "mail": "Service #2345"
-        },
-        "image": "https://images.unsplash.com/photo-1611162458324-aae1eb4129a4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg1fHxibGFjayUyMGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-        "name": "Flat White",
-        "ingredients": [
-            "Arabica",
-            "Slow brew",
-            "Oat milk"
-        ],
-        "orderId": "#34642",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Olivia Rhye",
-            "mail": "Service #2345"
-        },
-        "image": "https://images.unsplash.com/photo-1576092768241-dec231879fc3?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRlYXxlbnwwfHwwfHx8MA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-        "name": "Café Latte",
-        "ingredients": [
-            "Robusta",
-            "Soya milk"
-        ],
-        "orderId": "#33467",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Demi Wilkinson",
-            "mail": "demi@coffeUi.com"
-        },
-        "image": "https://images.unsplash.com/photo-1517701550927-30cf4ba1dba5?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aWNlZCUyMGxhdHRlfGVufDB8fDB8fHww",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-      "name": "Flat White",
-      "ingredients": [
-        "Arabica",
-        "Slow brew",
-        "Oat milk"
-      ],
-      "orderId": "#34642",
-      "paymentType": "Paid",
-      "executedBy": {
-        "name": "Olivia Rhye",
-        "mail": "Service #2345"
-      },
-      "image": "https://images.unsplash.com/photo-1642647391072-6a2416f048e5?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGljZWQlMjBtb2NoYSUyMGxhdHRlfGVufDB8fDB8fHww",
-      "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
   {
-    name: "Café Latte",
-    ingredients: ["Robusta", "Soya milk"],
-    orderId: "#33467",
+    name: "Flat White",
+    quantity: 3,
+    ingredients: ["Arabica", "Slow brew", "Oat milk"],
+    orderId: "#34642",
     paymentType: "Paid",
     executedBy: {
-      name: "Demi Wilkinson",
-      mail: "demi@coffeUi.com",
+      name: "Olivia Rhye",
+      mail: "Service #2345",
     },
-    {
-        "name": "Flat White",
-        "ingredients": [
-            "Arabica",
-            "Slow brew",
-            "Oat milk"
-        ],
-        "orderId": "#34642",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Olivia Rhye",
-            "mail": "Service #2345"
-        },
-        "image": "https://images.unsplash.com/photo-1494314671902-399b18174975?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
+    image:
+      "https://images.unsplash.com/photo-1578899952107-9c390f1af1b7?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNoYWklMjBsYXR0ZXxlbnwwfHwwfHx8MA%3D%3D",
+    description:
+      "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso.",
+  },
+  {
+    name: "Espresso",
+    quantity: 2,
+    ingredients: "Arabica",
+    orderId: "#12532",
+    paymentType: "Open",
+    executedBy: {
+      name: "Olivia Rhye",
+      mail: "Service #2345",
     },
-    {
-        "name": "Café Latte",
-        "ingredients": [
-            "Robusta",
-            "Soya milk"
-        ],
-        "orderId": "#33467",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Demi Wilkinson",
-            "mail": "demi@coffeUi.com"
-        },
-        "image": "https://images.unsplash.com/photo-1561882468-9110e03e0f78?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGxhdHRlfGVufDB8fDB8fHww",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-        "name": "Espresso",
-        "ingredients": "Arabica",
-        "orderId": "#12532",
-        "paymentType": "Open",
-        "executedBy": {
-            "name": "Olivia Rhye",
-            "mail": "Service #2345"
-        },
-        "image": "https://images.unsplash.com/photo-1599398054066-846f28917f38?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-        "name": "Café Latte",
-        "ingredients": [
-            "Robusta",
-            "Soya milk"
-        ],
-        "orderId": "#33467",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Demi Wilkinson",
-            "mail": "demi@coffeUi.com"
-        },
-        "image": "https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-        "name": "Café Latte",
-        "ingredients": [
-            "Robusta",
-            "Soya milk"
-        ],
-        "orderId": "#33467",
-        "paymentType": "Paid",
-        "executedBy": {
-            "name": "Demi Wilkinson",
-            "mail": "demi@coffeUi.com"
-        },
-        "image": "https://images.unsplash.com/photo-1532004491497-ba35c367d634?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    },
-    {
-        "name": "Espresso",
-        "ingredients": "Arabica",
-        "orderId": "#12532",
-        "paymentType": "Open",
-        "executedBy": {
-            "name": "Olivia Rhye",
-            "mail": "Service #2345"
-        },
-        "image": "https://images.unsplash.com/photo-1579992357154-faf4bde95b3d?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "description": "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso."
-    }
-    */
+    image:
+      "https://images.unsplash.com/photo-1536256263959-770b48d82b0a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bWF0Y2hhJTIwbGF0dGV8ZW58MHx8MHx8fDA%3D",
+    description:
+      "A flat white is a velvety espresso-based coffee drink originating from Australia or New Zealand. It's characterized by its smooth, creamy texture, achieved by microfoam steamed milk poured over a shot of espresso.",
+  },
 ];
 
 export { coffeeList };
